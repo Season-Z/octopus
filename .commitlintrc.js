11c15c997f08badc83412c6d2affa5d299bcac4f
zhouxishun <zhouxishun01@gmail.com>
2023-10-03 23:28:26 +0800

    refactor: 项目工程配置

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,8 +2,19 @@
  * @Author: zhouxishun
  * @Date: 2023-09-05 15:15:45
  * @LastEditors: zhouxishun
- * @LastEditTime: 2023-09-05 15:17:10
+ * @LastEditTime: 2023-10-03 23:28:21
  * @Description:
+ */
+/**
+- build: 影响构建系统或外部依赖项的更改（示例范围：gulp、broccoli、npm）
+- ci: 更改我们的 CI 配置文件和脚本（示例范围：Travis、Circle、BrowserStack、SauceLabs）
+- docs: 文档修改
+- feat: 一个新的功能
+- fix: 一个 bug 修复
+- perf: 提升性能的代码修改
+- refactor: 既不修复错误也不添加功能的代码更改
+- style: 不影响代码含义的更改（空格、格式、缺少分号等）
+- test: 添加缺失的测试或更正现有测试
  */
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
